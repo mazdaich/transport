e8668cac31e75449d6e41c468faa1ce7de54b246
sergey kuznetsov <mazdai.88@mail.ru>
2024-01-19 19:41:44 +0300

    отдельный модуль с ошибками создан у транспорта появилась проверка на ошибку LowFuel

--- Excep.py
+++ Excep.py
@@ -4,10 +4,9 @@ class LowFuel(Exception):
     def check_fuel(obg, distance):
         if (obg.fuel_in_tank - obg.consumption_fuel * distance) >= 0:
             return True
-        else:
-            raise LowFuel(
-                f'Недостаточно топлива\n'
-                f'Чтобы проехать {distance} км. нужно {obg.consumption_fuel * distance} л. топлива\n'
-                f'Сейчас в баке {obg.fuel_in_tank} л.,'
-                f" не хватает {format(obg.consumption_fuel * distance - obg.fuel_in_tank, '.2f')} л."
-            )
+        raise LowFuel(
+            f'Недостаточно топлива\n'
+            f'Чтобы проехать {distance} км. нужно {obg.consumption_fuel * distance} л. топлива\n'
+            f'Сейчас в баке {obg.fuel_in_tank} л.,'
+            f" не хватает {format(obg.consumption_fuel * distance - obg.fuel_in_tank, '.2f')} л."
+        )
