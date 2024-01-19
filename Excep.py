e64fd4d1c08b45d218a18a8fdc9575590d2f9798
sergey kuznetsov <mazdai.88@mail.ru>
2024-01-19 19:39:28 +0300

    отдельный модуль с ошибками создан у транспорта появилась проверка на ошибку LowFuel

---
+++ Excep.py
@@ -0,0 +1,13 @@
+
+class LowFuel(Exception):
+    @staticmethod
+    def check_fuel(obg, distance):
+        if (obg.fuel_in_tank - obg.consumption_fuel * distance) >= 0:
+            return True
+        else:
+            raise LowFuel(
+                f'Недостаточно топлива\n'
+                f'Чтобы проехать {distance} км. нужно {obg.consumption_fuel * distance} л. топлива\n'
+                f'Сейчас в баке {obg.fuel_in_tank} л.,'
+                f" не хватает {format(obg.consumption_fuel * distance - obg.fuel_in_tank, '.2f')} л."
+            )
--- all_transport.py
+++ all_transport.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 from playsound import playsound
+from Excep import LowFuel
 
 
 @dataclass
@@ -18,22 +19,15 @@ class Transport(ABC):
     def beep(self):
         pass
 
-    def check_fuel(self, distance):
-        return (self.fuel_in_tank - self.consumption_fuel * distance) >= 0
-
     def go(self, distance):
-        if self.check_fuel(distance):
+        try:
+            LowFuel.check_fuel(self, distance)
             #playsound('C:\\P.Projects\\Transport\\sound_for_python\\starting-p_car.mp3')  #звук должен быть изменяем КАК СДЕЛАТЬ?
             self.fuel_in_tank -= self.consumption_fuel * distance
-            print(f'Едем {distance} км. Израсходовали {self.consumption_fuel * distance} литров.\n'
-                  f''f'В баке осталось {self.fuel_in_tank} литров')
-        else:
-            print('Недостаточно топлива')
-            print(f'Чтобы проехать {distance}км. нужно {self.consumption_fuel * distance}л. топлива\n'
-                  f'Сейчас в баке {self.fuel_in_tank},'
-                  f' не хватает {self.consumption_fuel * distance - self.fuel_in_tank}л.')
-
-
+            print(f'Едем {distance} км. Израсходовали {self.consumption_fuel * distance} л.\n'
+                  f''f'В баке осталось {self.fuel_in_tank} л.')
+        except LowFuel as e:
+            print(e)
 
 
 if __name__ == '__main__':
